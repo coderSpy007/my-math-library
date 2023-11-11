c70c452c00dda71a1d2d90749d05fa33d4c0637f
aom <marisa.phann@dome.tu.ac.th>
2023-11-11 10:50:17 +0700

    Add sum array functionality

--- Downloads/my-math-library/math.js
+++ Downloads/my-math-library/math.js
@@ -4,3 +4,13 @@ function sum(a, b) {
 module.exports = { 
     sum,
 }
+function sum(a, b) {
+if (Array.isArray(a)) {
+let total = 0
+for (let i = 0; i < a.length; i++) {
+      total += a[i]
+    }
+    return total
+  } else if (typeof a === 'number' && typeof b === 'number') {
+return a + b }
+}
